2ead005b25df6436937034ba22759c33570bc922
alonrav74-sys <alonrav74@gmail.com>
2025-10-17 01:02:47 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,6 @@
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('chordfinder-cache').then(cache => {
-      return cache.addAll(['./', './index.html', './manifest.json']);
-    })
-  );
+  e.waitUntil(caches.open('chordfinder-v1').then(cache => cache.addAll(['/', '/index.html'])));
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
